cc89620e7bdf70f9720f3590333df96b094325d1
Tiflit <59609757+Tiflit@users.noreply.github.com>
2026-04-18 19:11:19 -0400

    Create dummy tray application with right-click menu
    Implement a Windows tray application with a context menu.

---
+++ dummy_tray.cpp
@@ -0,0 +1,77 @@
+#include <windows.h>
+#include <shellapi.h>
+
+#define WM_TRAY (WM_USER + 1)
+#define TRAY_ID 1
+
+static HICON g_icon = nullptr;
+static HWND  g_hwnd = nullptr;
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    if (msg == WM_TRAY)
+    {
+        if (LOWORD(lParam) == WM_RBUTTONUP)
+        {
+            HMENU menu = CreatePopupMenu();
+            AppendMenuW(menu, MF_STRING, 1, L"Exit");
+
+            POINT pt;
+            GetCursorPos(&pt);
+            SetForegroundWindow(hwnd);
+
+            int cmd = TrackPopupMenu(menu,
+                                     TPM_RETURNCMD | TPM_NONOTIFY | TPM_RIGHTBUTTON,
+                                     pt.x, pt.y, 0, hwnd, nullptr);
+
+            DestroyMenu(menu);
+
+            if (cmd == 1)
+                PostQuitMessage(0);
+        }
+        return 0;
+    }
+
+    return DefWindowProcW(hwnd, msg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int)
+{
+    g_icon = LoadIcon(nullptr, IDI_APPLICATION);
+
+    WNDCLASSW wc = {};
+    wc.lpfnWndProc   = WndProc;
+    wc.hInstance     = hInst;
+    wc.lpszClassName = L"DummyTrayClass";
+    RegisterClassW(&wc);
+
+    g_hwnd = CreateWindowExW(
+        WS_EX_TOOLWINDOW,
+        wc.lpszClassName,
+        L"",
+        WS_POPUP,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        nullptr, nullptr, hInst, nullptr
+    );
+
+    NOTIFYICONDATA nid = {};
+    nid.cbSize = sizeof(nid);
+    nid.hWnd = g_hwnd;
+    nid.uID = TRAY_ID;
+    nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+    nid.uCallbackMessage = WM_TRAY;
+    nid.hIcon = g_icon;
+    wcscpy_s(nid.szTip, L"Dummy Tray App");
+    Shell_NotifyIconW(NIM_ADD, &nid);
+
+    MSG msg;
+    while (GetMessageW(&msg, nullptr, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessageW(&msg);
+    }
+
+    Shell_NotifyIconW(NIM_DELETE, &nid);
+    return 0;
+}
