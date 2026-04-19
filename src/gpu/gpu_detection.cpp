37b3a33a6daa7cc294a61a51efdad463ffa39e09
Tiflit <59609757+Tiflit@users.noreply.github.com>
2026-04-19 16:22:54 -0400

    Implement ActivateRenderGPU function
    Added a new function to activate the render GPU using Direct3D 11.

--- src/gpu/gpu_detection.cpp
+++ src/gpu/gpu_detection.cpp
@@ -4,8 +4,6 @@
 #include <d3d11.h>
 #include <sstream>
 
-#include "../util/logging.h"
-
 #pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "d3d11.lib")
 
@@ -89,6 +87,30 @@ bool DetectRenderGPU(GpuState& outState)
     outState.vendor = desc.VendorId;
 
     return true;
+}
+
+bool ActivateRenderGPU()
+{
+    ID3D11Device* device = nullptr;
+    ID3D11DeviceContext* context = nullptr;
+
+    HRESULT hr = D3D11CreateDevice(
+        nullptr,
+        D3D_DRIVER_TYPE_HARDWARE,
+        nullptr,
+        0,
+        nullptr,
+        0,
+        D3D11_SDK_VERSION,
+        &device,
+        nullptr,
+        &context
+    );
+
+    if (context) context->Release();
+    if (device)  device->Release();
+
+    return SUCCEEDED(hr);
 }
 
 std::wstring BuildGpuTooltip(const GpuState& display, const GpuState& render)
