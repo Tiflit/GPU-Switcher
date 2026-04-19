cbb6d9dfe57914d943a652610706481b69bff635
Tiflit <59609757+Tiflit@users.noreply.github.com>
2026-04-19 13:49:43 -0400

    Add tray-related constants and WndProc declaration

--- src/win/win_helpers.h
+++ src/win/win_helpers.h
@@ -1,1 +1,12 @@
+#pragma once
 
+#include <Windows.h>
+
+// Tray-related constants
+constexpr UINT WM_TRAY          = WM_USER + 1;
+constexpr UINT TRAY_ID          = 1;
+constexpr UINT ID_RUN_AT_STARTUP = 1001;
+constexpr UINT ID_EXIT           = 1002;
+
+// Window procedure for the hidden tray window.
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
