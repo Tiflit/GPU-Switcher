e49d009b6ba73f0b68c42aa00d717e93d83d6e2b
Tiflit <59609757+Tiflit@users.noreply.github.com>
2026-04-19 13:47:39 -0400

    Declare LoadDisplayIcon function in tray_icon.h
    Added LoadDisplayIcon function declaration for GPU vendor icons.

--- src/tray/tray_icon.h
+++ src/tray/tray_icon.h
@@ -1,1 +1,7 @@
+#pragma once
 
+#include <Windows.h>
+
+// Returns an icon handle for the given GPU vendor.
+// If vendor == 0, a warning icon is returned.
+HICON LoadDisplayIcon(UINT vendor, HINSTANCE hInst);
