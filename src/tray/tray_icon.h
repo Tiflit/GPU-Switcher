d36748bcb85aa7d1ea6f3a4c5030952aa643029c
Tiflit <59609757+Tiflit@users.noreply.github.com>
2026-04-19 17:57:44 -0400

    Delete src/tray directory

--- src/tray/tray_icon.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <Windows.h>
-
-// Returns an icon handle for the given GPU vendor.
-// If vendor == 0, a warning icon is returned.
-HICON LoadDisplayIcon(UINT vendor, HINSTANCE hInst);
--- src/tray/tray_menu.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <Windows.h>
-
-// Shows the tray context menu and returns the selected command ID,
-// or 0 if nothing was selected.
-int ShowTrayMenu(HWND hwnd, bool startupEnabled, bool gpuError);
