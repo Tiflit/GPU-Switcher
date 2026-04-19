8fe7e544227cf0292c4652a5c76c6fa51591c7c0
Tiflit <59609757+Tiflit@users.noreply.github.com>
2026-04-19 14:22:36 -0400

    Add GPU-related constants and refresh function
    Added new tray-related constants and function declarations.

--- src/win/win_helpers.h
+++ src/win/win_helpers.h
@@ -2,11 +2,20 @@
 
 #include <Windows.h>
 
-// Tray-related constants
-constexpr UINT WM_TRAY          = WM_USER + 1;
-constexpr UINT TRAY_ID          = 1;
-constexpr UINT ID_RUN_AT_STARTUP = 1001;
-constexpr UINT ID_EXIT           = 1002;
+// Tray constants
+constexpr UINT WM_TRAY = WM_USER + 1;
+constexpr UINT TRAY_ID = 1;
 
-// Window procedure for the hidden tray window.
+constexpr UINT ID_RUN_AT_STARTUP     = 1001;
+constexpr UINT ID_EXIT               = 1002;
+constexpr UINT ID_TOGGLE_RENDER_GPU  = 2001;
+constexpr UINT ID_RESTART_GPU_DRIVER = 2002;
+
+// Provided by main.cpp
+extern bool g_forceRenderGpu;
+
+// Provided by main.cpp
+void RefreshGpuState(HWND hwnd);
+
+// Window procedure
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
