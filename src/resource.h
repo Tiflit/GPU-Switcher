4115c26df7e9420fbcc4ceae521ad77ef3b997f6
Tiflit <59609757+Tiflit@users.noreply.github.com>
2026-04-19 12:55:42 -0400

    Update icon IDs in resource.h

--- src/resource.h
+++ src/resource.h
@@ -1,5 +1,7 @@
-#define IDI_ICON_NVIDIA   101
-#define IDI_ICON_AMD      102
-#define IDI_ICON_INTEL    103
-#define IDI_ICON_UNKNOWN  104
-#define IDI_ICON_WARNING  105
+#pragma once
+
+#define IDI_ICON_INTEL    4000
+#define IDI_ICON_NVIDIA   4001
+#define IDI_ICON_AMD      4002
+#define IDI_ICON_UNKNOWN  4003
+#define IDI_ICON_WARNING  4004
