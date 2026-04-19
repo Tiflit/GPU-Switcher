edcbb2db44acb88fcb9295aece9e7dfb6391eb77
Tiflit <59609757+Tiflit@users.noreply.github.com>
2026-04-19 13:49:16 -0400

    Add startup management functions in startup.h

--- src/util/startup.h
+++ src/util/startup.h
@@ -1,1 +1,7 @@
+#pragma once
 
+// Returns true if the app is registered to run at user logon.
+bool IsStartupEnabled();
+
+// Enables or disables run-at-startup registration.
+void SetStartup(bool enable);
