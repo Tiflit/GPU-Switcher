cf69dd5e0b0e730df3f8567c1822d78969893ed8
Tiflit <59609757+Tiflit@users.noreply.github.com>
2026-04-19 15:33:21 -0400

    Update icon definitions in resource.h

--- src/resource.h
+++ src/resource.h
@@ -1,7 +1,11 @@
 #pragma once
 
-#define IDI_ICON_INTEL    4000
-#define IDI_ICON_NVIDIA   4001
-#define IDI_ICON_AMD      4002
-#define IDI_ICON_UNKNOWN  4003
-#define IDI_ICON_WARNING  4004
+// Application icon
+#define IDI_APP_ICON        1
+
+// GPU icons
+#define IDI_ICON_INTEL      100
+#define IDI_ICON_NVIDIA     101
+#define IDI_ICON_AMD        102
+#define IDI_ICON_UNKNOWN    103
+#define IDI_ICON_WARNING    104
