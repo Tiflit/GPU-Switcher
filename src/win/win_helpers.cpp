f1cfe699f013c638f85caa7aecaf41cee7211e5f
Tiflit <59609757+Tiflit@users.noreply.github.com>
2026-04-19 14:22:51 -0400

    Add GPU driver restart functionality and menu option

--- src/win/win_helpers.cpp
+++ src/win/win_helpers.cpp
@@ -3,27 +3,100 @@
 #include "../util/startup.h"
 #include "../tray/tray_menu.h"
 #include "../gpu/gpu_state.h"
+#include "../util/logging.h"
 
-// This will be defined in main.cpp
+// Globals from main.cpp
 extern GpuState g_displayGpuState;
+extern GpuState g_renderGpuState;
+extern bool g_forceRenderGpu;
+
+// Forward declaration from main.cpp
+extern void RefreshGpuState(HWND hwnd);
+
+// Restart GPU driver (same as Ctrl+Win+Shift+B)
+void RestartGpuDriver()
+{
+    // This triggers a graphics subsystem reset
+    // Equivalent to pressing Ctrl+Win+Shift+B
+    keybd_event(VK_CONTROL, 0, 0, 0);
+    keybd_event(VK_LWIN,    0, 0, 0);
+    keybd_event(VK_SHIFT,   0, 0, 0);
+    keybd_event('B',        0, 0, 0);
+
+    keybd_event('B',        0, KEYEVENTF_KEYUP, 0);
+    keybd_event(VK_SHIFT,   0, KEYEVENTF_KEYUP, 0);
+    keybd_event(VK_LWIN,    0, KEYEVENTF_KEYUP, 0);
+    keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
+}
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch (msg)
     {
+    case WM_TIMER:
+        // Periodic GPU refresh
+        RefreshGpuState(hwnd);
+        return 0;
+
     case WM_TRAY:
         if (LOWORD(lParam) == WM_RBUTTONUP)
         {
-            bool startup   = IsStartupEnabled();
-            bool gpuError  = (g_displayGpuState.vendor == 0);
+            HMENU menu = CreatePopupMenu();
 
-            int cmd = ShowTrayMenu(hwnd, startup, gpuError);
+            AppendMenuW(menu,
+                MF_STRING | (g_forceRenderGpu ? MF_CHECKED : 0),
+                ID_TOGGLE_RENDER_GPU,
+                L"Force dGPU rendering");
 
-            if (cmd == ID_RUN_AT_STARTUP)
-                SetStartup(!startup);
+            AppendMenuW(menu,
+                MF_STRING,
+                ID_RESTART_GPU_DRIVER,
+                L"Restart GPU driver");
 
-            if (cmd == ID_EXIT)
+            AppendMenuW(menu,
+                MF_STRING | (IsStartupEnabled() ? MF_CHECKED : 0),
+                ID_RUN_AT_STARTUP,
+                L"Run at startup");
+
+            AppendMenuW(menu,
+                MF_STRING,
+                ID_EXIT,
+                L"Exit");
+
+            POINT pt;
+            GetCursorPos(&pt);
+            SetForegroundWindow(hwnd);
+
+            int cmd = TrackPopupMenu(
+                menu,
+                TPM_RETURNCMD | TPM_NONOTIFY | TPM_RIGHTBUTTON,
+                pt.x, pt.y,
+                0,
+                hwnd,
+                nullptr
+            );
+
+            DestroyMenu(menu);
+
+            switch (cmd)
+            {
+            case ID_TOGGLE_RENDER_GPU:
+                g_forceRenderGpu = !g_forceRenderGpu;
+                RefreshGpuState(hwnd);
+                break;
+
+            case ID_RESTART_GPU_DRIVER:
+                RestartGpuDriver();
+                break;
+
+            case ID_RUN_AT_STARTUP:
+                SetStartup(!IsStartupEnabled());
+                break;
+
+            case ID_EXIT:
                 PostQuitMessage(0);
+                break;
+            }
         }
         return 0;
 
