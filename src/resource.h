24c1703ba9f36fcce1902dfa76e3d10d881260f3
Tiflit <59609757+Tiflit@users.noreply.github.com>
2026-04-19 20:17:02 -0400

    Reformat GPU icon definitions in resource.h

--- src/resource.h
+++ src/resource.h
@@ -1,11 +1,10 @@
 #pragma once
 
-// Application icon
-#define IDI_APP_ICON        1
+#define IDI_APP_ICON    1
 
-// GPU icons
-#define IDI_ICON_INTEL      100
-#define IDI_ICON_NVIDIA     101
-#define IDI_ICON_AMD        102
-#define IDI_ICON_UNKNOWN    103
-#define IDI_ICON_WARNING    104
+// Reserved for future vendor icon support
+#define IDI_ICON_INTEL   100
+#define IDI_ICON_NVIDIA  101
+#define IDI_ICON_AMD     102
+#define IDI_ICON_UNKNOWN 103
+#define IDI_ICON_WARNING 104
