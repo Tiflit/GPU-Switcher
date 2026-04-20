9eed712d19736a1dd80f593b99bfec666e2c3568
Tiflit <59609757+Tiflit@users.noreply.github.com>
2026-04-19 20:18:22 -0400

    Remove redundant comments in startup.h

--- src/util/startup.h
+++ src/util/startup.h
@@ -1,7 +1,4 @@
 #pragma once
 
-// Returns true if the app is registered to run at user logon.
 bool IsStartupEnabled();
-
-// Enables or disables run-at-startup registration.
 void SetStartup(bool enable);
