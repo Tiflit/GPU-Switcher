fba3ea50e7a1de0706c1bbae5b703315571c1acd
Tiflit <59609757+Tiflit@users.noreply.github.com>
2026-04-19 20:18:39 -0400

    Refactor startup functions to use constants

--- src/util/startup.cpp
+++ src/util/startup.cpp
@@ -1,21 +1,22 @@
 #include "startup.h"
+#include <windows.h>
 
-#include <Windows.h>
+static const wchar_t* kRunKey  = L"Software\\Microsoft\\Windows\\CurrentVersion\\Run";
+static const wchar_t* kValName = L"GPUSwitcher";
 
 bool IsStartupEnabled()
 {
     HKEY key;
-    if (RegOpenKeyExW(HKEY_CURRENT_USER,
-        L"Software\\Microsoft\\Windows\\CurrentVersion\\Run",
-        0, KEY_READ, &key) != ERROR_SUCCESS)
+    if (RegOpenKeyExW(HKEY_CURRENT_USER, kRunKey, 0, KEY_READ, &key) != ERROR_SUCCESS)
         return false;
 
-    wchar_t regPath[MAX_PATH], exePath[MAX_PATH];
+    wchar_t regPath[MAX_PATH] = {};
+    wchar_t exePath[MAX_PATH] = {};
     DWORD size = sizeof(regPath);
     bool match = false;
 
-    if (RegQueryValueExW(key, L"GPUSwitcher", nullptr, nullptr,
-        (BYTE*)regPath, &size) == ERROR_SUCCESS)
+    if (RegQueryValueExW(key, kValName, nullptr, nullptr,
+                         (BYTE*)regPath, &size) == ERROR_SUCCESS)
     {
         GetModuleFileNameW(nullptr, exePath, MAX_PATH);
         match = (_wcsicmp(regPath, exePath) == 0);
@@ -28,21 +29,19 @@ bool IsStartupEnabled()
 void SetStartup(bool enable)
 {
     HKEY key;
-    if (RegOpenKeyExW(HKEY_CURRENT_USER,
-        L"Software\\Microsoft\\Windows\\CurrentVersion\\Run",
-        0, KEY_SET_VALUE, &key) != ERROR_SUCCESS)
+    if (RegOpenKeyExW(HKEY_CURRENT_USER, kRunKey, 0, KEY_SET_VALUE, &key) != ERROR_SUCCESS)
         return;
 
     if (enable)
     {
-        wchar_t exePath[MAX_PATH];
+        wchar_t exePath[MAX_PATH] = {};
         GetModuleFileNameW(nullptr, exePath, MAX_PATH);
-        RegSetValueExW(key, L"GPUSwitcher", 0, REG_SZ,
+        RegSetValueExW(key, kValName, 0, REG_SZ,
             (BYTE*)exePath, (DWORD)((wcslen(exePath) + 1) * sizeof(wchar_t)));
     }
     else
     {
-        RegDeleteValueW(key, L"GPUSwitcher");
+        RegDeleteValueW(key, kValName);
     }
 
     RegCloseKey(key);
